f731bf14d3d535e54b15a3a1974a9327058b040d
455607047@qq.com <455607047@qq.com>
2019-04-12 22:29:28 +0800

    创建工程

---
+++ src/main/java/com/gd/spike/SpikeApplication.java
@@ -0,0 +1,13 @@
+package com.gd.spike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gd/spike/SpikeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gd.spike;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpikeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
