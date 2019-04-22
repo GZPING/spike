45ece9feecdfd79958f8c2c33dfdd0436f9cdfe7
455607047@qq.com <455607047@qq.com>
2019-04-22 21:25:16 +0800

    测试

---
+++ src/main/java/com/gd/spike/base/beans/ResultBean.java
@@ -0,0 +1,30 @@
+package com.gd.spike.base.beans;
+
+/**
+ * Created by GD on 2019/4/22.
+ */
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @Description TODO
+ * @Author GD
+ * @Date 2019/4/22 21:22
+ * @Version 1.0V
+ */
+
+@Data
+public class ResultBean<T> implements Serializable {
+
+    private boolean success;
+
+    private T data;
+
+    public ResultBean(T data) {
+        this.success = true;
+        this.data = data;
+    }
+}
--- src/main/java/com/gd/spike/controller/OrdersController.java
+++ src/main/java/com/gd/spike/controller/OrdersController.java
@@ -1,6 +1,7 @@
 package com.gd.spike.controller;
 
 
+import com.gd.spike.base.beans.ResultBean;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -17,4 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/spike/orders")
 public class OrdersController {
 
+    public ResultBean<String> test(){
+        return new ResultBean<>("ok");
+    }
 }
