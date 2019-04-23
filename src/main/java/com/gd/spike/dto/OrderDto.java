88d6509e440899c5b597516be9ad80d124f5aa6f
455607047@qq.com <455607047@qq.com>
2019-04-23 23:10:21 +0800

    order

---
+++ src/main/java/com/gd/spike/dto/OrderDto.java
@@ -0,0 +1,21 @@
+package com.gd.spike.dto;
+
+/**
+ * Created by GD on 2019/4/23.
+ */
+
+import lombok.Data;
+
+/**
+ *
+ * @Description TODO
+ * @Author GD
+ * @Date 2019/4/23 23:09
+ * @Version 1.0V
+ */
+
+@Data
+public class OrderDto {
+
+
+}
