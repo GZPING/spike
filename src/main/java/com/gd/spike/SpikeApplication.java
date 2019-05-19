4b356cfdc93f841cd644400b9f73d72ec279f757
455607047@qq.com <455607047@qq.com>
2019-05-19 23:34:16 +0800

    redis

--- src/main/java/com/gd/spike/SpikeApplication.java
+++ src/main/java/com/gd/spike/SpikeApplication.java
@@ -1,11 +1,12 @@
 package com.gd.spike;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
+@MapperScan("com.gd.spike.mapper")
 public class SpikeApplication {
 
     public static void main(String[] args) {
